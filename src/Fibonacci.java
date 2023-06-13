d48ce6143ebafbd1f784df0ad5fa662a2351b2ef
whereisdiamond <whereisdiamond@gmail.com>
2023-06-13 19:22:07 -0400

    This adds javadoc

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,20 +1,23 @@
 public class Fibonacci {
 
     /**
-     *
      * @param n
-     * @return
+     * this is the Recursion Method
      */
 
+
     //fiboRecur allows for the fibonacci sequence to be generated using the recursion method
-    public static int fiboRecur(int n){
+    public static int fiboRecur(int n) {
         if (n <= 1)
             return n;
 
 
-        int fibCurr = fiboRecur(n - 1) + fiboRecur(n - 2);
-        return fibCurr;
+        return fiboRecur(n - 1) + fiboRecur(n - 2);
     }
+    /**
+     * @param n
+     * this Method prints the recursion outputs
+     */
     //When I try to print using fiboRecur, it always prints a 1 between each number, so I used this to filter it out.
     public static void printFiboRecur(int n) {
         for (int i = 0; i < n; i++) {
@@ -27,39 +30,45 @@ public class Fibonacci {
     }
 
     //This method generates the fibonacci sequence iteratively
-    public static int fibonacciIterative(int n) {
-        if (n <= 1)
-            return n;
-
+    /**
+     * @param n
+     * this is the Iterative method
+     */
+    public static void printFiboIterative(int n) {
         int fibPrev = 0;
         int fibCurr = 1;
 
-        System.out.print(fibPrev + " " + fibCurr + " ");
+        System.out.print(fibPrev + " ");
+        long startTime = System.nanoTime();
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibCurr + " ");
+
             int temp = fibPrev + fibCurr;
-            System.out.print(temp + " ");
             fibPrev = fibCurr;
             fibCurr = temp;
+            long endTime = System.nanoTime();
+            long executionTime = endTime - startTime;
+            System.out.println("Execution time: " + executionTime + " nanoseconds");
         }
-
-        return fibCurr;
     }
-//This is the main function
-    public static void main(String args[]) {
-        //Recursive Method
+    /**
+     * @param args
+     * this is the Main Method
+     */
+    //This is the main function
+    public static void main(String[] args) {
         int n = 10;
         System.out.print("Recursive Fibonacci: ");
         printFiboRecur(n);
         System.out.println();
 
-        //Iterative Method
-        long startTime = System.nanoTime();
-        int fibIterative = fibonacciIterative(n);
-        long endTime = System.nanoTime();
+        System.out.print("Iterative Fibonacci: ");
+        printFiboIterative(n);
+        System.out.println();
 
-        long executionTime = endTime - startTime;
+        System.out.println("hello world");
 
-        System.out.println("Iterative Fibonacci: " + fibIterative);
     }
+
 }
